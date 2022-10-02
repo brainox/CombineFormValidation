84fc25eb6ce0358d8a387bf33ee5d392e7acfac8
Obinna <obinna.aguwa@gmail.com>
2022-10-02 02:15:08 +0100

    Initial Commit

---
+++ CombineFormValidation/CombineFormValidationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineFormValidationApp.swift
+//  CombineFormValidation
+//
+//  Created by Obinna Aguwa on 02/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct CombineFormValidationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
